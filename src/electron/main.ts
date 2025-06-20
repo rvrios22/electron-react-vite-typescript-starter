bcb2a45c110c1b46b113e675997ef831873ccadd
Rafael Rios <rvrios22@gmail.com>
2025-06-20 05:52:31 -0700

    Update main.ts

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,6 +2,16 @@ import { app, BrowserWindow } from 'electron'
 import path from 'path'
 import isDev from './utils/isDev.js'
 import { getPreloadPath } from './utils/pathResolver.js'
+/* 
+  This if check remove the following error from constantly running in the terminal.
+  EGL Driver message (Error) eglQueryDeviceAttribEXT: Bad attribute.
+  There may be a performance hit depending on your application.
+  Remove if necessary, does not affect app building if its not there, just removes unneccesary erros in terminal.
+*/
+import os from 'os';
+if (os.platform() === 'darwin' && os.arch() === 'x64') {
+  app.disableHardwareAcceleration();
+}
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({
         webPreferences: {
